be9e4e1dbd882d3b8662af9339c5fa1cb7fa9421
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-07 12:09:04 +0000

    now using sessions to store data in between requests

--- app.rb
+++ app.rb
@@ -1,13 +1,25 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/players' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+  post '/names' do
+    session[:player_1] = params[:player_1]
+    p session[:player_1]
+
+    session[:player_2] = params[:player_2]
+    p session[:player_2]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:players)
   end
 
