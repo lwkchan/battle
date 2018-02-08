f50774107d36b1ff4cfe5f72466ed17670aae315
Laura Chan <laurachan205@gmail.com>
2018-02-08 15:13:33 +0000

    Games.new accepts two new player instances

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,18 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double('player_1')                      }
+  let(:player_2) { double('player_2')                      }
 
+  context 'when initialized' do
+    it 'assigns a player instance to player_1 attribute' do
+      expect(game.player_1).to eq(player_1)
+    end
+    it 'assigns a player instance to player_2 attribute' do
+      expect(game.player_2).to eq(player_2)
+    end
+  end
 
   describe '#attack' do
     it 'calls damage on the player' do
