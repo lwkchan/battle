5e33b659853622646a26099cacf4800f7c66f0ea
Laura Chan <laurachan205@gmail.com>
2018-02-08 10:59:37 +0000

    Pass unit tests for 'attacking player 2' feature

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,21 @@
 class Player
 
   MAX_HP = 100
+  DEFAULT_DAMAGE = 10
 
   attr_reader :name, :hit_points
 
   def initialize(name)
     @name = name
     @hit_points = MAX_HP
+  end
+
+  def attack(player)
+    player.damage
+  end
+
+  def damage
+    @hit_points -= DEFAULT_DAMAGE
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,14 +2,30 @@ require 'player'
 
 describe Player do
 
-  subject(:player) { described_class.new('Lola') }
+  subject(:player1) { described_class.new('Lola') }
+  subject(:player2) { described_class.new('Julie') }
+
 
   context 'when initialized' do
     it 'has a name' do
-      expect(player.name).to eq 'Lola'
+      expect(player1.name).to eq 'Lola'
     end
     it 'has maximum hit points' do
-      expect(player.hit_points).to eq Player::MAX_HP
+      expect(player1.hit_points).to eq Player::MAX_HP
+    end
+  end
+
+  describe '#attack' do
+    it 'calls #damage on the other player' do
+      expect(player2).to receive(:damage)
+      player1.attack(player2)
+    end
+  end
+
+  describe 'when attacked' do
+    it 'loses 10 HP' do
+      player1.attack(player2)
+      expect(player2.hit_points).to eq Player::MAX_HP - Player::DEFAULT_DAMAGE
     end
   end
 
